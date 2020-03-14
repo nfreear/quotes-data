38a6cddeb2491233022d99d3675c438f6ae30632
Nick Freear <nfreear@yahoo.co.uk>
2020-03-14 10:41:23 +0000

    Working on `index.js`

--- index.js
+++ index.js
@@ -3,14 +3,32 @@
  * @author NDF, 09-March-2020.
  */
 
-const AUTHOR_SHORT_LIST = require('./data/short-list');
+const SHORT_LIST = require('./data/short-list');
 
-module.exports = {
-  OPEN_QUOTES_EN: mergeAuthorQuotes (AUTHOR_SHORT_LIST)
+const OPEN_QUOTES_EN = {
+  meta: SHORT_LIST.meta,
+  shortList: SHORT_LIST.data,
+  data: mergeAuthorQuotes (SHORT_LIST),
 };
 
-// @TODO:
-function mergeAuthorQuotes(shortList) {
+module.exports = { OPEN_QUOTES_EN };
 
-  return shortList;
+// --------------------------------------------
+
+function mergeAuthorQuotes(shortList) {
+  let allQuotes = [];
+
+  shortList.data.forEach(author => {
+    const QUOTES = require(`./data/short-list/${author.id}-quotes.json`);
+
+    const quotes = QUOTES.quotes.map(quote => {
+      return { id: quote.id, en: quote.en, name: QUOTES.name };
+    });
+
+    allQuotes = [ ...allQuotes, ...quotes ];
+  });
+
+  console.log('Quotes:', allQuotes);
+
+  return allQuotes;
 }
